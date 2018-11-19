780bf4a383f17cb58510610cba77db2a97eafa3a
Kane Cheshire <kane.cheshire@googlemail.com>
2018-11-19 08:45:23 +0000

    Initial Commit

---
+++ XCResultViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XCResultViewer
+//
+//  Created by Kane Cheshire on 19/11/2018.
+//  Copyright © 2018 kane.codes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XCResultViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCResultViewer
+//
+//  Created by Kane Cheshire on 19/11/2018.
+//  Copyright © 2018 kane.codes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
